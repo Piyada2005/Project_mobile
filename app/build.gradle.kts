015b2aef8a755f60d8cd0477ba7d9eca798af9d4
Piyada2005 <piya050510@gmail.com>
2026-02-26 01:56:10 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 }
 
 dependencies {
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation(libs.androidx.core.ktx)
